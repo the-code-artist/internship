6e511dbb131ba6c5d9d84df2fdd715caa544edd2
PAVAN KUMAR MARPU <pavankumarmarpu23@gmail.com>
2023-08-25 18:57:21 +0530

    division

--- Operations.java
+++ Operations.java
@@ -5,16 +5,15 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Operations
-{
-	public static void operation(String operation, int a, int b){
+class Operations {
+	public static void operation(String operation, int a, int b) {
 		System.out.println(operation);
-		System.out.println(a+b);
-		
+		System.out.println(a - b);
+
 	}
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		operation("+",1,2);
+
+	public static void main(String[] args) throws java.lang.Exception {
+		operation("-", 1, 2);
 		// your code goes here
 	}
 }
